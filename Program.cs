b7c93bc543879b07f20eb03442d46955a464aa5f
Anirudha Gupta <35772421+anirugu@users.noreply.github.com>
2022-12-23 23:41:29 +0530

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿namespace BuildArrayfromPermutationCsharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
+
+public class Solution
+{
+    public int[] BuildArray(int[] nums)
+    {
+        
+    }
+}
